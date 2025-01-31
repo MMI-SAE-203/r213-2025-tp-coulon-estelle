9436f2da6b0e5ea1f0ffaf6ac82465903ab42ac7
coulon-estelle <estelle.coulon@edu.univ-fcomte.fr>
2025-01-31 09:46:31 +0100

    Add PocketBase integration and refactor offres page to fetch data dynamically

---
+++ src/js/backend.mjs
@@ -0,0 +1,18 @@
+import PocketBase from "pocketbase";
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function getOffres() {
+    try {
+        let data = await pb.collection('Maison').getFullList({
+            sort: '-created',
+        });
+        data = data.map((Maison) => {
+            Maison.img= pb.files.getURL(Maison, Maison.imgUrl);
+            return Maison;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
